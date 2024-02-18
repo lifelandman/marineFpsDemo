74bdef9c10a117681caa45d1bd32272d19ebba9a
lifelandman <jacksonjams3@gmail.com>
2024-02-17 21:25:59 -0700

    Lots of early work.
    basically worked on the __init__ function, nothing special yet.

--- code/entities/playerEnts.py
+++ code/entities/playerEnts.py
@@ -3,10 +3,15 @@ All the "player" classes.
 '''
 
 ####Player
-#panda3d's:
+###panda3d's:
+#General:
+from panda3d.core import NodePath
+#collision:
 from panda3d.core import CollisionNode, CollisionBox, CollisionSphere
 from panda3d.core import Point3
-#ours:
+#Bullet raytracing:
+from panda3d.core import LensNode, PerspectiveLens
+###ours:
 from .npEnt import npEnt
 
 class playerEnt(npEnt):
@@ -28,9 +33,41 @@ class playerEnt(npEnt):
         self.wBall = self.np.attach_new_node(cNode)
         del cNode
         
+        #Create bullet LensNode.
+        '''
+        Panda3d let's you extrude vectors from a lens based on coordinates. we can use this to calculate bullet raycasting vectors. 
+        '''
+        self._rig = NodePath(self.name + "_rig")#this is the axis of pitch rotation for both the raycast LensNode and the camera. We don't just parent the camera to the LensNode because we might want an offset for the bullet origin.
+        self._rig.reparent_to(self.np)
+        self._rig.set_z(0.9)
+
+        self._bulletNode = LensNode(self.name + "_bulletLens", PerspectiveLens())#Weapons will modify these properties when they're set active.
+        self._bulletLens = self._bulletNode.get_lens()
+        
+        ################Create all instance variables##############
+        
+        #Administrative/anti-cheat
+        self.teleportOk = False#Set this to true if and whenever velosity's big enough or we're teleporting somewhere. If not this and this player moved too far, it's possible that client is trying to lie about present location.
+        
+        #Movement
+        self._xMove = 0.0 #float for relatively left/right movement.
+        self._yMove = 0.0 #ditto, but for forward/back
+        
+        self._wantJump = False#does this player object want to jump whenever such an action is valid?
+        self._wantCrouch = False#will we even use this right away?
+        
+        self._hRot = 0.0 #amount of heading rotation. If we're doing mouse/keys, this is how far along the x axis from the center user has moved cursor.
+        self._pRot = 0.0 #pitch rotation. (looking up/down.)
+        
         self.wBall.show()
         self.bBox.show()
-        "we create some instance variables used to calculate movement"
+        
+    def spawn(self, sPoint: NodePath):
+        self.np.reparent_to(base.render)
+        self.np.set_pos(sPoint, 0,0,0)
+    
+    def de_spawn(self):
+        self.np.remove_node()#carefull not to lose the class refrence during this time!
         
     def update(self):
         pass
