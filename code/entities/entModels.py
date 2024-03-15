e42f224643cbf6ea24e01b67ace02537165256ba
lifelandman <jacksonjams3@gmail.com>
2024-03-14 19:03:49 -0600

    started new thing
    even if this doesn't work, the knowledge is too valuable to lose

--- code/entities/entModels.py
+++ code/entities/entModels.py
@@ -18,6 +18,7 @@ class modelEnt (npEnt):
     
     modelPath = 'box'
     
+    
     def __init__(self, np, pos = (0,0,0), **kwargs):
         mnp = loader.loadModel(self.modelPath)
         if np != None:
@@ -61,3 +62,53 @@ class modelInstanceEnt (npEnt):
 from .funcs import funcSpin
 class spinningModel(modelInstanceEnt, funcSpin):
     pass
+
+
+##BIIG STUFF. BIG BIG BIG!!!
+
+from panda3d.core import LoaderOptions, Character, PartSubset, auto_bind, AnimControlCollection
+from direct.directnotify.DirectNotify import DirectNotify
+'''
+Okay, here's a refrence of stuff:
+Character: the node that holds the geometry, and updates the animation
+PartSubset: we can use this to define part of the character we want to control independantly
+AnimControl: The class that controlls the animation.
+auto_bind: creates AnimControls for all parts for all visible animations inside the egg file
+'''
+class playerAnimModBase(npEnt):
+    modelLoadOps = LoaderOptions(LoaderOptions.LF_search | LoaderOptions.LF_report_errors | LoaderOptions.LF_convert_skeleton)
+    
+    parts = {}#partname: ((include part names), (exclude part names))
+    
+    npOursOverrideable = True
+    
+    modelPath = 'player1'
+    
+    notify = DirectNotify.newCategory("anim_warning")
+    
+    def __init__(self, np, pos = (0,0,0), **kwargs):
+        mnp = loader.loadModel(self.modelPath, self.modelLoadOps)
+        if np != None:
+            mnp.reparent_to(np)#We hijack the np parameter and use it as our parent
+        self.character = mnp.find("**/+Character")
+        mnp.set_pos(*pos)
+        kwargs['np'] = mnp
+        super().__init__(**kwargs)#Trick npEnt into holding the root of our model
+        
+        #Character stuff
+        self.controls = {"modelRoot" : AnimControlCollection()}
+        auto_bind(self.character, self.controls["modelRoot"])
+        
+
+        if len(self.parts) <= 1:
+            bundle = self.character.get_bundle(0)
+            for part in self.parts.keys():
+                partCont = AnimControlCollection()
+                for anim in self.mnp.find("**/+AnimBundleNode"):
+                    sPart = PartSubset()
+                    for include in self.parts[part][0]:
+                        sPart.add_include_joint(include)
+                    for exclude in self.parts[part][1]:
+                        sPart.add_exclude_joint(exclude)
+                    partCont.store_anim(bundle.bind_anim(anim.get_bundle(), 0x01 | 0x02, sPart), anim.get_bundle().get_name())
+                self.controls[part] = partCont#Store the new animControlCollection under the name of the part
